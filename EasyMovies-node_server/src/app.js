f9ce59f253fa5dc5fcfa653c09dbc4d804cb8a9b
Thomas Rigoni <thomas.rigoni7@gmail.com>
2020-11-09 19:04:46 +0100

    prova per risposte multiple

--- EasyMovies-node_server/src/app.js
+++ EasyMovies-node_server/src/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Some HTML</h1>');
-  res.send('<p>Even more HTML</p>');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/users/register', (req, res) => {
+  const user = req.body;
+  var isPresent = false;
+  
+  //TODO:controlla se il nome è già presente
+  //try/catch
+  if(isPresent){
+    res.status(409);
+    res.send();
+  }
+  else{
+    users.push(user);
+    res.status(201);
+    res.send("{sessionID:'fvhusighfrvg'}");
+  }
+
+  //finally
+  res.status(500);
+  res.send();
 });
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
