4499ed892f3204397c4eb89aba9ab7b71f98eb4d
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-11-18 21:18:13 +0100

    utilizzo della libreria testCommons e aggionamento dei test

--- EasyMovies-node_server/src/tests/movies.test.js
+++ EasyMovies-node_server/src/tests/movies.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
-const app = require("../app");
 const dbAdapter = require('../libs/dbAdapter');
+const testCommons = require('./testCommons');
 
 
 describe("Test the movies api", () => {
@@ -9,29 +9,18 @@ describe("Test the movies api", () => {
 
     let cookies;
 
-    beforeAll(async (done) => {
-        const port = process.env.PORT || 5000;
-        return new Promise(async (resolve, reject) => {
-            await dbAdapter.initDB().catch((e) => reject(e));
-            server = app.listen(port, () => {
-                console.log(`DB connected and server listening on port ${port}`);
-                agent = request.agent(server);
-                done();
-            });
-        });
+    beforeAll((done)=>{testCommons.testConnect(done).then(r=>{server = r.server; agent = r.agent})});
+
+    afterAll((done) => {testCommons.testClose(done, server)});
+
+    test("200 on get movie data with existing movie", async (done) => {
+        const response = await agent.get('/movies/tt0000009')
+        expect(response.status).toBe(200);
+        done()
     });
 
-    afterAll((done) => {
-        server.close(() => {
-            dbAdapter.closeDB().then(() => { console.log("Server and DB connection closed"); done(); })
-        });
-    });
-
-    test("200 on get movie data with existing movie", () => {
-        agent.get("/movies/tt0000009").expect(200);
-    });
-
-    test("404 on get movie data with non-existing movie", () => {
-        agent.get("/movies/fake-movie").expect(404);
+    test("404 on get movie data with non-existing movie", async (done) => {
+        const response = await agent.get("/movies/fake-movie")
+        expect(response.status).toBe(404);
     });
 });
