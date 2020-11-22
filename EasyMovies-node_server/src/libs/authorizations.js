48bd51c9280f440eeda47d1a864792c019728b9c
Sebastian <n.sebastian.sani@gmail.com>
2020-11-22 21:55:17 +0100

    piccoli fix in funzione authorization

--- EasyMovies-node_server/src/libs/authorizations.js
+++ EasyMovies-node_server/src/libs/authorizations.js
@@ -5,11 +5,11 @@ const dbAdapter = require('./dbAdapter');
 var router = express.Router();
 
 const authorizationCallBack = async (req, res, next) => {
-    const username = req.params.username;
+    const username = req.params.username || req.body.username;
     if (dataChecker.checkFieldsNull([username]))
         res.status(400).send();
     if(!(await dataChecker.checkUsername(username))){
-        res.sendStatus(404);
+        res.status(404).send();
         return;
     }
     if (req.cookies && req.cookies.sessionId) {
