838f29b32b5a8be7d82d51bbe8a8f0bc46783634
Thomas Rigoni <thomas.rigoni7@gmail.com>
2020-11-23 09:48:41 +0100

    riorganizzato ordine test di users e usato test-user-username

--- EasyMovies-node_server/src/tests/users.test.js
+++ EasyMovies-node_server/src/tests/users.test.js
@@ -16,69 +16,6 @@ describe("Test the users api", () => {
 
     afterAll((done) => { testCommons.testClose(done, server) });
 
-
-
-    
-    //GET /users/username
-
-    test("200 on get user data with existing user", async(done) => {
-        const response = await agent.get('/users/aaaa');
-        expect(response.status).toBe(200);
-        done()
-    });
-    
-    test("404 on get user data with non-existing user", async(done) => {
-        const response = await agent.get("/users/fake-user");
-        expect(response.status).toBe(404);
-        done();
-    });
-
-    //POST /users/login
-
-    test("400 on login with no password", async(done) => {
-        const response1 = await agent.post("/users/login").send({ username: "aaaa"});
-        expect(response1.status).toBe(400);
-        done();
-    });
-
-    test("404 on login with non-existing username", async(done) => {
-        const response1 = await agent.post("/users/login").send({ username: "bbbb", password: "bbbb" });
-        expect(response1.status).toBe(404);
-        done();
-    });
-
-    test("401 on login with wrong password", async(done) => {
-        const response1 = await agent.post("/users/login").send({ username: "aaaa", password: "bbbb" });
-        expect(response1.status).toBe(401);
-        done();
-    });
-
-    test("200 on successful login", async(done) => {
-        const response1 = await agent.post("/users/login").send({ username: "aaaa", password: "aaaa" });
-        expect(response1.status).toBe(200);
-        done();
-    });
-
-    //POST /users/logout
-
-    test("404 on log out with wrong username", async(done) => {
-        const response = await agent.post("/users/bbbb/logout");
-        expect(response.status).toBe(404);
-        done();
-    });
-    
-    test("200 on successful logout", async(done) => {
-        const response = await agent.post("/users/aaaa/logout");
-        expect(response.status).toBe(200);
-        done();
-    });
-
-    test("401 on log out without logging in", async(done) => {
-        const response = await agent.post("/users/aaaa/logout");
-        expect(response.status).toBe(401);
-        done();
-    });
-
     //POST /users/register
 
     test("201 on successful user creation", async(done) => {
@@ -112,33 +49,92 @@ describe("Test the users api", () => {
         expect(response.status).toBe(409);
         done();
     });
+    
+    //GET /users/username
+
+    test("200 on get user data with existing user", async(done) => {
+        const response = await agent.get('/users/test-user-username');
+        expect(response.status).toBe(200);
+        done()
+    });
+    
+    test("404 on get user data with non-existing user", async(done) => {
+        const response = await agent.get("/users/fake-user");
+        expect(response.status).toBe(404);
+        done();
+    });
+
+    //POST /users/login
+
+    test("400 on login with no password", async(done) => {
+        const response1 = await agent.post("/users/login").send({ username: "test-user-username"});
+        expect(response1.status).toBe(400);
+        done();
+    });
+
+    test("404 on login with non-existing username", async(done) => {
+        const response1 = await agent.post("/users/login").send({ username: "fake-user", password: "fake-pass" });
+        expect(response1.status).toBe(404);
+        done();
+    });
+
+    test("401 on login with wrong password", async(done) => {
+        const response1 = await agent.post("/users/login").send({ username: "test-user-username", password: "test-user-wrong-password" });
+        expect(response1.status).toBe(401);
+        done();
+    });
+
+    test("200 on successful login", async(done) => {
+        const response1 = await agent.post("/users/login").send({ username: "test-user-username", password: "test-user-password" });
+        expect(response1.status).toBe(200);
+        done();
+    });
+
+    //POST /users/logout
+
+    test("404 on log out with wrong username", async(done) => {
+        const response = await agent.post("/users/fake-user/logout");
+        expect(response.status).toBe(404);
+        done();
+    });
+    
+    test("200 on successful logout", async(done) => {
+        const response = await agent.post("/users/test-user-username/logout");
+        expect(response.status).toBe(200);
+        done();
+    });
+
+    test("401 on log out without logging in", async(done) => {
+        const response = await agent.post("/users/test-user-username/logout");
+        expect(response.status).toBe(401);
+        done();
+    });
 
     //GET /users/username/reviews
 
     test("200 on successful user reviews get", async(done) => {
-        const response = await agent.get("/users/aaaa/reviews");
+        const response = await agent.get("/users/test-user-username/reviews");
         expect(response.status).toBe(200);
         done();
     });
 
     test("404 on user reviews get with wrong user", async(done) => {
-        const response = await agent.get("/users/bbbb/reviews");
+        const response = await agent.get("/users/fake-user/reviews");
         expect(response.status).toBe(404);
         done();
     });
 
     //DELETE /users/username
+    test("401 on user deletion with no authorization", async(done) => {
+        const response = await agent.delete("/users/test-user-username");
+        expect(response.status).toBe(401);
+        done();
+    });
 
     test("200 on successful user deletion", async(done) => {
         await agent.post("/users/login").send({ username: "test-user-username", password: "test-user-password" });
         const response = await agent.delete("/users/test-user-username");
         expect(response.status).toBe(200);
-        done();
-    });
-
-    test("401 on user deletion with no authorization", async(done) => {
-        const response = await agent.delete("/users/aaaa");
-        expect(response.status).toBe(401);
         done();
     });
 
