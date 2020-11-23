b40c1673ae905b506c68632ae2099f1f22349b58
paolobarzon2 <paolo.barzon@studenti.unitn.it>
2020-11-23 09:23:01 +0100

    fix di review test

--- EasyMovies-node_server/src/tests/reviews.test.js
+++ EasyMovies-node_server/src/tests/reviews.test.js
@@ -7,7 +7,32 @@ describe("Test the reviews api", () => {
 
     let server, agent;
 
-    beforeAll((done)=>{testCommons.testConnect(done).then(r=>{server = r.server; agent = r.agent})});
+    beforeAll((done)=>{
+        testCommons.testConnect(done).then( async r => {
+            server = r.server; agent = r.agent;
+            try {
+                await agent.post("/users/register").send({
+                    name: "test-user",
+                    password: "test-user-password",
+                    surname: "test-user-surname",
+                    username: "test-user"
+                });
+            } catch (e) {
+                //Do nothing
+            }
+            try {
+                await agent.post("/users/register").send({
+                    name: "second-test-user",
+                    password: "second-test-user-password",
+                    surname: "second-test-user-surname",
+                    username: "second-test-user"
+                });
+            } catch (e) {
+                //Do nothing
+            }
+            done();
+        });
+    });
 
     afterAll((done) => {testCommons.testClose(done, server)});
 
@@ -23,38 +48,32 @@ describe("Test the reviews api", () => {
         done();
     });
 
-    test("400 on put movie review without authorization without cookies", async(done) => {
-        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "aaaa", title: "title", content: "content", rate: 5 });
-        expect(response1.status).toBe(400);
-        done();
-    });
-
-    test("404 on put movie review on non existing movie", async(done) => {
-        const response1 = await agent.put("/movies/not-a-movie/reviews").send({ username: "aaaa", title: "title", content: "content", rate: 5 });
-        expect(response1.status).toBe(404);
+    test("401 on put movie review without authorization without cookies", async(done) => {
+        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "test-user", title: "title", content: "content", rate: 5 });
+        expect(response1.status).toBe(401);
         done();
     });
 
     test("401 on delete movie review without authorization without cookies", async(done) => {
-        const response1 = await agent.delete("/movies/tt0000009/reviews").send({ username: "aaaa"});
+        const response1 = await agent.delete("/movies/tt0000009/reviews").send({ username: "test-user"});
         expect(response1.status).toBe(401);
         done();
     });
 
     test("401 on patch movie review without authorization", async(done) => {
-        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "aaaa", title: "new-title", content: "different-content", rate: 4 });
+        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "test-user", title: "new-title", content: "different-content", rate: 4 });
         expect(response1.status).toBe(401);
         done();
     });
 
     test("200 on successful login", async(done) => {
-        const response1 = await agent.post("/users/login").send({ username: "aaaa", password: "aaaa" });
+        const response1 = await agent.post("/users/login").send({ username: "test-user", password: "test-user-password" });
         expect(response1.status).toBe(200);
         done();
     });
 
     test("201 on succesful put movie review", async(done) => {
-        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "aaaa", title: "title", content: "content", rate: 5 });
+        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "test-user", title: "title", content: "content", rate: 5 });
         expect(response1.status).toBe(201);
         done();
     });
@@ -66,59 +85,71 @@ describe("Test the reviews api", () => {
     });
 
     test("400 on put movie review with invalid data", async(done) => {
-        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "aaaa", title: "title", content: "content", rate: -100 });
+        const response1 = await agent.put("/movies/tt0000009/reviews").send({ username: "test-user", title: "title", content: "content", rate: -100 });
         expect(response1.status).toBe(400);
         done();
     });
 
-    test("401 on delete movie review without authorization", async(done) => {
-        const response1 = await agent.delete("/movies/tt0000009/reviews").send({ username: "aaa"});
-        expect(response1.status).toBe(401);
-        done();
-    });
-
-    test("404 on delete non-existing-movie review", async(done) => {
-        //Movie does not exist
-        const response1 = await agent.delete("/movies/not-a-movie/reviews").send({ username: "aaaa"});
-        expect(response1.status).toBe(404);
-        done();
-    });
-
-    test("404 on delete non-existing movie-review", async(done) => {
-        //Review does not exist
-        const response1 = await agent.delete("/movies/tt0000574/reviews").send({ username: "aaaa"});
+    test("404 on put movie review on non existing movie", async(done) => {
+        const response1 = await agent.put("/movies/not-a-movie/reviews").send({ username: "test-user", title: "title", content: "content", rate: 5 });
         expect(response1.status).toBe(404);
         done();
     });
 
     test("200 on succesful patch movie review", async(done) => {
-        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "aaaa", title: "new-title", content: "different-content", rate: 4 });
+        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "test-user", title: "new-title", content: "different-content", rate: 4 });
         expect(response1.status).toBe(200);
         done();
     });
 
     test("401 on patch movie review without authorization", async(done) => {
-        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "aaa", title: "new-title", content: "different-content", rate: 4 });
+        const response1 = await agent.patch("/movies/tt0000009/reviews").send({ username: "second-test-user", title: "new-title", content: "different-content", rate: 4 });
         expect(response1.status).toBe(401);
         done();
     });
 
     test("404 on patch non-existing movie-review", async(done) => {
         //Review does not exist
-        const response1 = await agent.patch("/movies/tt0000574/reviews").send({ username: "aaaa", title: "title", content: "content", rate: 5 });
+        const response1 = await agent.patch("/movies/tt0000574/reviews").send({ username: "test-user", title: "title", content: "content", rate: 5 });
         expect(response1.status).toBe(404);
         done();
     });
 
     test("404 on patch non-existing-movie review", async(done) => {
         //Movie does not exist
-        const response1 = await agent.patch("/movies/not-a-movie/reviews").send({ username: "aaaa", title: "title", content: "content", rate: 5 });
+        const response1 = await agent.patch("/movies/not-a-movie/reviews").send({ username: "test-user", title: "title", content: "content", rate: 5 });
+        expect(response1.status).toBe(404);
+        done();
+    });
+
+    test("200 on succesful delete movie review", async(done) => {
+        const response1 = await agent.delete("/movies/tt0000009/reviews").send({ username: "test-user"});
+        expect(response1.status).toBe(200);
+        done();
+    });
+
+    test("401 on delete movie review without authorization", async(done) => {
+        const response1 = await agent.delete("/movies/tt0000009/reviews").send({ username: "second-test-user"});
+        expect(response1.status).toBe(401);
+        done();
+    });
+
+    test("404 on delete non-existing-movie review", async(done) => {
+        //Movie does not exist
+        const response1 = await agent.delete("/movies/not-a-movie/reviews").send({ username: "test-user"});
+        expect(response1.status).toBe(404);
+        done();
+    });
+
+    test("404 on delete non-existing movie-review", async(done) => {
+        //Review does not exist
+        const response1 = await agent.delete("/movies/tt0000574/reviews").send({ username: "test-user"});
         expect(response1.status).toBe(404);
         done();
     });
     
     test("200 on successful logout", async(done) => {
-        const response = await agent.post("/users/aaaa/logout");
+        const response = await agent.post("/users/test-user/logout");
         expect(response.status).toBe(200);
         done();
     });
