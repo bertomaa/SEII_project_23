83c592f000f67c46a71594c4086f2f49336c35ad
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-11-30 15:48:21 +0100

    inizializzato router per react app

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Homepage from "./Components/Homepage/Homepage"
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Route exact path="/" component={Homepage}></Route>
+    </Router>
+);
 }
 
 export default App;
