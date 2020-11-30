b11c381ea76f36d58ad6f084a06d3dbe79f04640
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-11-30 15:23:27 +0100

    aggiunta antd come design library

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary">Button</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
