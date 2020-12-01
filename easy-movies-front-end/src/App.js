add441dc33ae61b709aeab37e21ac45eb4e1f9d5
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-01 14:11:46 +0100

    risolto un bug relativo allo sfondo

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import style from './Generic.module.css';
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
@@ -13,12 +14,15 @@ function App() {
   const [keyword, setKeyword] = useState("");
 
   return (
+    <>
+    <div className={style.background} />
     <Router>
       <Route path="/"><TopBar onChange={setKeyword} /></Route>
       <Route exact path="/" component={Homepage}></Route>
       {/* <Route path="/search"><SearchMovies keyword={keyword} /></Route>
       <Route exact path="/MovieDetails/:id" component={MovieDetails} /> */}
     </Router>
+    </>
   );
 }
 
