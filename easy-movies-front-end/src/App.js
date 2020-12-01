cfed47bcfe6f50f3ffdd07e4692eef135c9c287f
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-01 15:46:35 +0100

    aggiornato il file .env

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -3,7 +3,9 @@ import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
-import Homepage from "./Components/Homepage/Homepage"
+import Homepage from "./Components/Homepage/Homepage";
+require('dotenv').config()
+
 
 function App() {
   return (
