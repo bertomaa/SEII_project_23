616adbe3863dc33f8d9f987328b002d4d274eaab
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-01 10:16:56 +0100

    sistemato un problema con cors che non permetteva al front end di effettuare delle richieste al backend

--- EasyMovies-node_server/src/app.js
+++ EasyMovies-node_server/src/app.js
@@ -17,6 +17,12 @@ console.log("USING ENVIRONMENT: " + process.env.NODE_ENV);
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let authorizationRouter = express.Router();
 var routerApiV1 = express.Router();
 var routerApiV2 = express.Router();
