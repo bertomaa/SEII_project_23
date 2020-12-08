4a722a42b3f48226290c2db7a2d2364cb214d166
Thomas Rigoni <thomas.rigoni7@gmail.com>
2020-12-08 15:22:54 +0100

    tolte stampe e aggiornata logica di rendering

--- easy-movies-front-end/src/Components/MovieDetails/MovieDetails.js
+++ easy-movies-front-end/src/Components/MovieDetails/MovieDetails.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useContext } from 'react';
 import style from "./MovieDetails.module.css";
 import Axios from 'axios';
 
-export default function MovieDetails({match}) {
-    
+export default function MovieDetails({ match }) {
+
     var _ = require('lodash');
-    
+
     const [movie, setMovie] = useState({});
     const [trailer, setTrailer] = useState("");
     const [poster, setPoster] = useState("");
@@ -19,15 +19,13 @@ export default function MovieDetails({match}) {
         setIsLoadingTrailer(true)
         setIsLoadingPoster(true)
         Axios.get(`http://localhost:5000/api/v2/movies/${match.params.movieId}`).then((res) => {
-            console.log("==================================================================================");
-            console.log(res);
             let tmp = {};
             tmp.title = _.get(res.data, "title", "Titolo sconosciuto");
             tmp.runtime = _.get(res.data, "runtime", "sconosciuta");
             tmp.release_date = _.get(res.data, "release_date", "sconosciuta");
             tmp.vote_average = _.get(res.data, "vote_average", -1);
             tmp.overview = _.get(res.data, "overview", "Trama sconosciuta");
-            tmp.genres = _.get(res.data, "genres", [{name:"Genere sconosciuto"}]);
+            tmp.genres = _.get(res.data, "genres", [{ name: "Genere sconosciuto" }]);
             setMovie(tmp);
             setIsLoadingMovie(false);
 
@@ -36,9 +34,13 @@ export default function MovieDetails({match}) {
                 setIsLoadingTrailer(false);
             }
 
-            setPoster(_.get(res.data, "poster_path"),"");
-            setIsLoadingPoster(false);
-        }).catch(e=>console.log(e))
+            if (res.data.poster_path) {
+                setPoster(_.get(res.data, "poster_path"), "");
+                setIsLoadingPoster(false);
+            }
+
+
+        }).catch(e => console.log(e))
     }, [match.params.movieId, _]);
 
     const mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
@@ -54,24 +56,24 @@ export default function MovieDetails({match}) {
         <>
             <div className={style.container}>
                 {!isLoadingTrailer &&
-                    <div style={{padding:"0 10vw", marginTop:"20px"}}>
+                    <div style={{ padding: "0 10vw", marginTop: "20px" }}>
                         <div className={style.videoContainer}>
                             <iframe
                                 src={trailer}
                                 frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen>
                             </iframe>
                         </div>
                     </div>
                 }
-                {!isLoadingPoster && !isLoadingMovie &&
+                {!isLoadingMovie &&
                     <div className={style.movie}>
-                        <img className={style.poster} src={"https://image.tmdb.org/t/p/original/" + poster} alt={movie.title} />
+                        {!isLoadingPoster && <img className={style.poster} src={"https://image.tmdb.org/t/p/original/" + poster} alt={movie.title} />}
                         <div className={style.details}>
                             <p className={style.title}>{movie.title}</p>
                             <div className={style.data}>
-                <p>Durata: {movie.runtime} {movie.runtime === "sconosciuta"? "" : "minuti"}</p>
+                                <p>Durata: {movie.runtime} {movie.runtime === "sconosciuta" ? "" : "minuti"}</p>
                                 <p>Uscita: {movie.release_date === "sconosciuta" ? movie.release_date : showDate(movie.release_date)}</p>
                             </div>
 
@@ -82,8 +84,8 @@ export default function MovieDetails({match}) {
                         </div>
                     </div>
                 }
-                </div>
-            
+            </div>
+
         </>
     );
 }
