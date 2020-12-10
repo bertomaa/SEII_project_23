d93814a3e0ded56fed9104885aaa0692b22bf769
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-10 20:17:39 +0100

    Revert "WOP public key"
    This reverts commit d4a8b9b023c78e0ed4596d6afde66ca01867c3a0.

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -7,32 +7,16 @@ import {
 } from "react-router-dom";
 import TopBar from "./Components/TopBar/TopBar.js";
 import Cookies from 'universal-cookie';
-var jwt = require('jsonwebtoken');
-
-const publicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCBmLWXNgo2w4WkuqR3ieJa/KV3
-InwwHp8fd+7DmHas1u/vuE2C/DJWOxNydCcrH7N5GjZmlfZUeGUq6WplbXFAkmLF
-V8wixqxdJmVTBxRDvJU9+d85cC063wVFyt2HQwwmhNATwCJXfynMcQ7WKcmg+KZB
-9MKbzV3cubBTtbc/VwIDAQAB`
 
 export const AuthContext = React.createContext({
   username: undefined,
-  setUsername: () => { }
+  setUsername: () => {}
 });
 
-async function App() {
+function App() {
   const cookies = new Cookies();
-  let storedUsername;
-  if (cookies.get("JWTtoken")) {
-    storedUsername = jwt.verify(cookies.get("JWTtoken"), publicKey, (error, decode) => {
-      console.log(decode.username)
-      if (!!error || !!decode.username)
-        storedUsername = undefined
-      else
-        storedUsername = decode.username
-    });
-  }
-
-  const [username, setUsername] = useState("storedUsername");
+  
+  const [username, setUsername] = useState(cookies.get("JWTtoken") ? "aaaa" : undefined);
   const value = { username, setUsername };
 
 
