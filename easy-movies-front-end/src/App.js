d80467a2e3c805e1a85a6c32f8afef176ec6e3f4
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-14 16:18:11 +0100

    rifiniti gli ultimi dettagli riguardo lo stato del login

--- easy-movies-front-end/src/App.js
+++ easy-movies-front-end/src/App.js
@@ -7,18 +7,25 @@ import {
 } from "react-router-dom";
 import TopBar from "./Components/TopBar/TopBar.js";
 import Cookies from 'universal-cookie';
+var jwt = require('jsonwebtoken');
+
+const publicKey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCBmLWXNgo2w4WkuqR3ieJa/KV3
+InwwHp8fd+7DmHas1u/vuE2C/DJWOxNydCcrH7N5GjZmlfZUeGUq6WplbXFAkmLF
+V8wixqxdJmVTBxRDvJU9+d85cC063wVFyt2HQwwmhNATwCJXfynMcQ7WKcmg+KZB
+9MKbzV3cubBTtbc/VwIDAQAB
+-----END PUBLIC KEY-----`
 
 export const AuthContext = React.createContext({
   username: undefined,
-  setUsername: () => {}
+  setUsername: () => { }
 });
 
 function App() {
   const cookies = new Cookies();
-  
-  const [username, setUsername] = useState(cookies.get("JWTtoken") ? "aaaa" : undefined);
-  const value = { username, setUsername };
 
+  const [username, setUsername] = useState(cookies.get("username"));
+  const value = { username, setUsername };
 
   return (
     <AuthContext.Provider value={value}>
--- easy-movies-front-end/src/Components/TopBar/TopBar.js
+++ easy-movies-front-end/src/Components/TopBar/TopBar.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./TopBar.module.css"
 import SearchBar from '../SearchBar/SearchBar';
-import { HomeFilled, HomeOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeFilled, HomeOutlined, MenuOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import FlexView from 'react-flexview/lib';
 import Avatar from 'antd/lib/avatar/avatar';
 import { Popover, message } from 'antd';
 import classNames from 'classnames';
 import { AiOutlineUserAdd } from 'react-icons/ai';
-import { Input, Divider, Button } from 'antd';
+import { Input, Divider, Button, Upload } from 'antd';
 import Axios from 'axios';
 import Cookies from 'universal-cookie';
 import { AuthContext } from '../../App';
@@ -20,11 +20,28 @@ export default function TopBar(props) {
 	const size = useWindowSize();
 	const [mobile, setMobile] = useState(false);
 	const { username, setUsername } = useContext(AuthContext);
-	let usernameForm, password, name, surname;
+	const [usernameForm, setUsernameForm] = useState(undefined)
+	const [usernameFormLogin, setUsernameFormLogin] = useState(undefined)
+	const [passwordLogin, setPasswordLogin] = useState(undefined)
+	const [password, setPassword] = useState(undefined)
+	const [name, setName] = useState(undefined)
+	const [nameLogged, setNameLogged] = useState(undefined)
+	const [surnameLogged, setSurnameLogged] = useState(undefined)
+	const [surname, setSurname] = useState(undefined)
+	const [imgBase64, setImgBase64] = useState(undefined)
+
+	
 
 	useEffect(() => {
 		setMobile(size.width < 600)
 	}, [size])
+
+	useEffect(()=>{
+		Axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v2/users/${username}`).then(res=>{
+			setNameLogged(res.data.name)
+			setSurnameLogged(res.data.surname)
+		})
+	}, [username])
 
 	const register = () => {
 		Axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/v2/users/register`, {
@@ -32,14 +49,13 @@ export default function TopBar(props) {
 			"password": password,
 			"name": name,
 			"surname": surname,
+			"image": imgBase64,
 		}).then(res => {
-			cookies.set('JWTtoken', res.data.JWTtoken, { path: '/' });
-			setUsername("aaaa");
-			console.log(username);
-		}).catch((res)=>{
-			switch(res.status){
+			login();
+		}).catch((res) => {
+			switch (res.status) {
 				//TODO: mettere i vari casi
-				default: 
+				default:
 					console.error(res)
 					message.error("Si è verificato un errore durante la registrazione")
 					break;
@@ -49,19 +65,20 @@ export default function TopBar(props) {
 
 	const login = () => {
 		Axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/v2/users/login`, {
-			"username": usernameForm,
-			"password": password
+			"username": usernameFormLogin,
+			"password": passwordLogin
 		}).then(res => {
 			cookies.set('JWTtoken', res.data.JWTtoken, { path: '/' });
-			setUsername("aaaa");
+			cookies.set('username', res.data.username, { path: '/' });
+			setUsername(res.data.username);
 			console.log(username);
-		}).catch((res)=>{
-			switch(res.status){
+		}).catch((res) => {
+			switch (res.status) {
 				//TODO: mettere i vari casi
 				case "401":
 					message.error("Credenziali errate")
 					break;
-				default: 
+				default:
 					console.error(res)
 					message.error("Si è verificato un errore durante il login")
 					break;
@@ -71,38 +88,42 @@ export default function TopBar(props) {
 
 	const logout = () => {
 		cookies.remove("JWTtoken");
+		cookies.remove("username");
 		setUsername(undefined);
 	}
 
 	const registerPopover = (
 		<FlexView column style={{ alignItems: "center" }}>
-			<Avatar icon={<AiOutlineUserAdd />} style={{ marginBottom: "10px", flexShrink: 0, fontSize: "30px", padding: "10px", width: "50px", height: "50px" }} />
-			<Input placeholder="name" style={{ margin: "10 10px" }} onChange={e => name = e.target.value} />
+			<FlexView style={{ alignItems: "center" }}>
+				<AvatarUpload setImg={(r)=>{setImgBase64(r)}} style={{ marginBottom: "10px", flexShrink: 0, fontSize: "30px", padding: "10px", width: "50px", height: "50px" }} />
+			</FlexView>
+			{/* <Avatar icon={<AiOutlineUserAdd />} style={{ marginBottom: "10px", flexShrink: 0, fontSize: "30px", padding: "10px", width: "50px", height: "50px" }} /> */}
+			<Input placeholder="name" style={{ margin: "10 10px" }} onChange={e => setName(e.target.value)} />
 			<div style={{ height: "10px" }} />
-			<Input placeholder="surname" style={{ margin: "10 10px" }} onChange={e => surname = e.target.value} />
+			<Input placeholder="surname" style={{ margin: "10 10px" }} onChange={e => setSurname(e.target.value)} />
 			<div style={{ height: "10px" }} />
-			<Input placeholder="username" style={{ margin: "10 10px" }} onChange={e => usernameForm = e.target.value} />
+			<Input placeholder="username" style={{ margin: "10 10px" }} onChange={e => setUsernameForm(e.target.value)} />
 			<div style={{ height: "10px" }} />
-			<Input.Password placeholder="password" type="password" style={{ margin: "10 10px" }} onChange={e => password = e.target.value} />
+			<Input.Password placeholder="password" type="password" style={{ margin: "10 10px" }} onChange={e => setPassword(e.target.value)} />
 			<Divider />
-			<Button type="primary" style={{ width: "100%" }} onClick={register}>Registrati</Button>
+			<Button type="primary" style={{ width: "100%" }} onClick={register} disabled={!(usernameForm && password && name && surname && imgBase64)}>Registrati</Button>
 		</FlexView>
 	);
 
 	const loginPopover = (
 		<FlexView column style={{ alignItems: "center" }}>
-			<Input placeholder="username" style={{ margin: "10 10px" }} onChange={e => usernameForm = e.target.value}/>
+			<Input placeholder="username" style={{ margin: "10 10px" }} onChange={e => setUsernameFormLogin(e.target.value)} />
 			<div style={{ height: "10px" }} />
-			<Input.Password placeholder="password" type="password" style={{ margin: "10 10px" }} onChange={e => password = e.target.value}/>
+			<Input.Password placeholder="password" type="password" style={{ margin: "10 10px" }} onChange={e => setPasswordLogin(e.target.value)} />
 			<Divider />
-			<Button type="primary" style={{ width: "100%" }} onClick={login}>Accedi</Button>
+			<Button type="primary" style={{ width: "100%" }} onClick={login} disabled={!(usernameFormLogin && passwordLogin)}>Accedi</Button>
 		</FlexView>
 	);
 
 	const accountPopover = (
 		<FlexView column style={{ alignItems: "center" }}>
-			<Avatar icon={<AiOutlineUserAdd />} style={{ marginBottom: "10px", flexShrink: 0, fontSize: "30px", padding: "10px", width: "50px", height: "50px" }} />
-			<div>Utente</div>
+			<Avatar src={`${process.env.REACT_APP_API_BASE_URL}/profile-images/${username}.jpg`} icon={<AiOutlineUserAdd />} style={{ marginBottom: "10px", flexShrink: 0, fontSize: "30px", padding: "10px", width: "50px", height: "50px" }} />
+			<div>{nameLogged + " " + surnameLogged}</div>
 			<Button type="primary" style={{ width: "100%" }} onClick={logout}>Disconnetti</Button>
 		</FlexView>
 	);
@@ -124,8 +145,8 @@ export default function TopBar(props) {
 						</Popover>
 					</FlexView>
 					:
-					<Popover content={accountPopover} title={"nome account"}>
-						<Avatar className={classNames(styles.topBarElement, styles.accountIcon, { [styles.accountIconMobile]: mobile })} >A</Avatar>
+					<Popover content={accountPopover} title={username}>
+						<Avatar className={classNames(styles.topBarElement, styles.accountIcon, { [styles.accountIconMobile]: mobile })} src={`${process.env.REACT_APP_API_BASE_URL}/profile-images/${username}.jpg`}></Avatar>
 					</Popover>
 				}
 			</FlexView>
@@ -160,4 +181,70 @@ function useWindowSize() {
 		return () => window.removeEventListener('resize', handleResize);
 	}); // Empty array ensures that effect is only run on mount and unmount
 	return windowSize;
+}
+
+class AvatarUpload extends React.Component {
+	state = {
+		loading: false,
+	};
+
+	handleBase64 = imageUrl => {
+		this.setState({
+			imageUrl,
+			loading: false,
+		});
+		this.props.setImg(imageUrl);
+	}
+
+	handleChange = info => {
+		if (info.file.status === 'uploading') {
+			this.setState({ loading: true });
+			return;
+		}
+		if (info.file.status === 'done') {
+			// Get this url from response in real world.
+			getBase64(info.file.originFileObj, this.handleBase64);
+		}
+	};
+
+	render() {
+		const { loading, imageUrl } = this.state;
+		const uploadButton = (
+			<div>
+				{loading ? <LoadingOutlined /> : <AiOutlineUserAdd style={{ fontSize: "24px" }} />}
+				<div style={{ marginTop: 8 }}>Upload</div>
+			</div>
+		);
+		return (
+			<Upload
+				name="avatar"
+				listType="picture-card"
+				className="avatar-uploader"
+				showUploadList={false}
+				action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+				beforeUpload={beforeUpload}
+				onChange={this.handleChange}
+			>
+				{imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+			</Upload>
+		);
+	}
+}
+
+function getBase64(img, callback) {
+	const reader = new FileReader();
+	reader.addEventListener('load', () => callback(reader.result));
+	reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+	const isJpg = file.type === 'image/jpeg';
+	if (!isJpg) {
+		message.error('Puoi caricare solo file JPG');
+	}
+	const isLt2M = file.size / 1024 / 1024 < 4;
+	if (!isLt2M) {
+		message.error('Puoi caricare solo file con dimensione inferiore a 4 MB');
+	}
+	return isJpg && isLt2M;
 }
