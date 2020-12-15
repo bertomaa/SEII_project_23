3323cbfa3e7444c9791255a3b590eb5eece89715
Marco Bertolino <marcobertolino.developer@gmail.com>
2020-12-15 11:40:28 +0100

    risolto problema relativo a cors

--- EasyMovies-node_server/src/app.js
+++ EasyMovies-node_server/src/app.js
@@ -24,8 +24,12 @@ const app = express();
 // app.use(cors())
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  if(req.method == "OPTIONS"){
+    res.status(200).send();
+  } else
   next();
 });
 
