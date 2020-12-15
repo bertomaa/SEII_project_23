38606ebd76020d26bcda9140f1a320fbbb6f97e8
Thomas Rigoni <thomas.rigoni7@gmail.com>
2020-12-15 14:08:17 +0100

    aggiornata origine cors

--- EasyMovies-node_server/src/app.js
+++ EasyMovies-node_server/src/app.js
@@ -19,11 +19,17 @@ const PORT = process.env.PORT || 5001;
 console.log("USING ENVIRONMENT: " + process.env.NODE_ENV);
 console.log("USING API KEY    : " + process.env.TMDB_API_KEY);
 
+let XOrigin = "";
+if(process.env.NODE_ENV == "production")
+  XOrigin = "https://se2-client.herokuapp.com"
+else
+  XOrigin = "http://localhost:3000"
+
 const app = express();
 
 // app.use(cors())
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", Xorigin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
